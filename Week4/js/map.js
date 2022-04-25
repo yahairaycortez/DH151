46377582a8bba3bb71a199420344978b40a68652
yahairaycortez <yahairaycortez@gmail.com>
2022-04-24 19:02:50 -0700

    updates

--- Week4/js/map.js
+++ Week4/js/map.js
@@ -3,16 +3,10 @@ let map;
 let lat = 0;
 let lon = 0;
 let zl = 3;
-let path = "data/dunitz.csv";
-// global variables
-let markers = L.featureGroup();
-
-
 
 // initialize
 $( document ).ready(function() {
-	createMap(lat,lon,zl);
-	readCSV(path);
+    createMap(lat,lon,zl);
 });
 
 // create the map
@@ -23,57 +17,3 @@ function createMap(lat,lon,zl){
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 }
-
-// function to read csv data
-function readCSV(path){
-	Papa.parse(path, {
-		header: true,
-		download: true,
-		complete: function(data) {
-			console.log(data);
-			
-			// map the data
-			mapCSV(data);
-
-		}
-	});
-}
-
-function mapCSV(data){
-
-	// circle options
-		let circleOptions = {
-			radius: 5,
-			weight: 1,
-			color: 'white',
-			fillColor: 'dodgerblue',
-			fillOpacity: 1
-		}
-	
-		// loop through each entry
-		data.data.forEach(function(item,index){
-			// create a marker
-			let marker = L.circleMarker([item.latitude,item.longitude],circleOptions)
-			.on('mouseover',function(){
-				this.bindPopup(`${item.title}<br><img src="${item.thumbnail_url}">`).openPopup()
-			})
-	
-			// add marker to featuregroup
-			markers.addLayer(marker)
-	
-			
-		})
-	
-		// add featuregroup to map
-		markers.addTo(map)
-	
-		// fit map to markers
-		map.fitBounds(markers.getBounds())
-	}
-
-	function panToImage(index){
-		// zoom to level 17 first
-		map.setZoom(17);
-		// pan to the marker
-		map.panTo(markers.getLayers()[index]._latlng);
-	}
